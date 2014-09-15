e103d22687642c527414dfaa5b46bf6f7ad61701
Alan <alan.vey12@imperial.ac.uk>
2014-09-15 19:17:20 +0100

    Skeleton Structure

--- generate_data.rb
+++ generate_data.rb
@@ -1,5 +1,54 @@
 def generate_data
 end
 
+
+# Level 2 =====================================================================
 def process_data
 end
+
+def generate_competitor_ratios(processed_data)
+end
+
+def generate_historical_ratios(processed_data)
+end
+
+# Level 3 =====================================================================
+def process_company_data(cik)
+end
+
+def get_ciks
+end
+
+# Level 4 =====================================================================
+def process_company_annual_data(cik, year)
+end
+
+# Level 5 =====================================================================
+def calculate_ratios(path)
+end
+
+# Level 6 =====================================================================
+def extract_data(path)
+end
+
+def liquidity_ratios(extracted_data)
+end
+
+def debt_ratios(extracted_data)
+end
+
+def profitability_ratios(extracted_data)
+end
+
+def cash_flow_ratios(extracted_data)
+end
+
+def operating_performance_ratios(extracted_data)
+end
+
+def valuation_ratios(extracted_data)
+end
+
+# Level 6 =====================================================================
+# All individual ratio calculation
+
