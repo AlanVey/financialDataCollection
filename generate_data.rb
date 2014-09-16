012d0ea5ab00fca33e07609de2c519206153efd1
Alan <alan.vey12@imperial.ac.uk>
2014-09-16 14:36:42 +0100

    CCC implemented

--- generate_data.rb
+++ generate_data.rb
@@ -210,6 +210,15 @@ end
 def cash_conversion_cycle(start_inventory, end_inventory, 
   start_accounts_receivable, end_accounts_receivable, start_accounts_payable, 
   end_accounts_payable, cogs, net_sales)
+  accounts_receivable = start_accounts_receivable.to_f/end_accounts_receivable
+  accounts_payable    = start_accounts_payable.to_f/end_accounts_payable
+  inventory           = start_inventory.to_f/end_inventory
+
+  dio                 = inventory/(cogs/365.25)
+  dso                 = accounts_receivable/(net_sales/365.25)  
+  dpo                 = accounts_payable/(cogs/365.25)
+
+  dio+dso-dpo
 end
 
 #------------------------------------------------------------------------------
