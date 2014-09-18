ae26f212431f812b530dfe0b4b0cc52ca256572a
Alan <alan.vey12@imperial.ac.uk>
2014-09-18 22:40:47 +0100

    Last bit

--- analysis/ratios/cash_flow_ratios.rb
+++ analysis/ratios/cash_flow_ratios.rb
@@ -1,9 +1,9 @@
 def cash_flow_ratios(data)
   cash_flow_data = Array.new
 
-  ocf     = data["ocf"][0]
-  revenue = data["revenue"][0]
-  capex   = data["capex"][0]
+  ocf     = data["ocf"][0].to_f
+  revenue = data["revenue"][0].to_f
+  capex   = data["capex"][0].to_f
 
 
   cash_flow_data << ocf_to_sales(ocf, revenue)
@@ -16,13 +16,13 @@ end
 # Ratios ======================================================================
 
 def ocf_to_sales(ocf, revenue)
-  ocf.to_f/revenue
+  ocf/revenue
 end
 
 def fcf_to_ocf(fcf, ocf)
-  fcf.to_f/ocf
+  fcf/ocf
 end
 
 def capex_coverage(ocf, capex)
-  ocf.to_f/capital_expenditure
+  ocf/capex
 end
