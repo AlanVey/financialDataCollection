303a9b23bc2a1b7ce418b2af367e6cb88c0b6790
Alan <alan.vey12@imperial.ac.uk>
2014-09-18 14:40:47 +0100

    find_tags()

--- analysis/find_XBRL_tags.rb
+++ analysis/find_XBRL_tags.rb
@@ -12,43 +12,23 @@ def print_tag_stats_to_file
   end
 end
 
-def good_tags(tags_data, array_of_regex)
-  best_tags = Hash.new
-
-  (2009..Time.now.year).each do |year|
-    num_accounts = tags_data[year.to_s]["NUMACCOUNTS"]
-    regex_hash   = Hash.new
-
-    array_of_regex.each do |regex|
-      regex_hash[regex.to_s] = Array.new
-      tags_data[year.to_s][regex.to_s].each do |tag|
-        regex_hash[regex.to_s] << tag[0] if tag[1] == num_accounts
-      end
-    end
-    best_tags[year.to_s] = regex_hash
-  end
-  best_tags
-end
-
 def find_tags(xbrl_tags)
   tags_data = Hash.new
 
-  (2009..Time.now.year).each do |year|
+  (2010..Time.now.year).each do |year|
     puts "Reading tags for #{year}..."
-
     total_hash                = Hash.new
     total_hash["NUMACCOUNTS"] = 0
+
     (1..12).each do |m|
       print "  Reading tags for #{m}..."
-
       month = month_convert(sprintf('%02d', m))
 
       Dir["../download/sec/#{year}/#{month}/*/*"].each do |file_path|
         if file_path =~ /\d+[^_].xml$/
           open_xbrl = Xbrlware.ins(file_path).item_all_map.keys
           total_hash["NUMACCOUNTS"] += 1
-
-          total_hash = find_matching_tags(xbrl_tags, total_hash, open_xbrl)
+          total_hash = find_matching_priority_tags(xbrl_tags, total_hash, open_xbrl)
         end
       end
       print "Done\n"
@@ -59,10 +39,19 @@ def find_tags(xbrl_tags)
   tags_data
 end
 
-def find_matching_tags(tags, hash, keys)
-  tags.each do |tag|
-    if keys.include?(tag)
-      hash[tag] == nil ? hash[tag] = 1 : hash[tag] += 1
+def find_matching_priority_tags(tags_hash, hash, keys)
+  tags_hash.each do |key, priority_list|
+    matched_priority = 100
+    for i in 0..(priority_list.length - 1)
+      if (priority_list[i] - keys).empty?
+        matched_priority = i 
+        break
+      end
+    end
+
+    if matched_priority != 100
+      hash[key] = Array.new(priority_list.length, 0) if hash[key] == nil
+      hash[key][matched_priority] += 1
     end
   end 
   hash
