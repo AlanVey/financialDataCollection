b4cf2546066f42f2121640068574e2f5d613c1f1
Annika Monari <annikamonari@gmail.com>
2014-09-23 20:50:23 +0100

    dkjflds

--- screen/process_data.rb
+++ screen/process_data.rb
@@ -94,9 +94,9 @@ def rank_valuation_ratios(data)
 	ranked_comps = { 'price_to_sales' => [], 'PEG' => [], 'EBITDA' => [] }
 	data.collect do |comp|
 		comp_info = [comp[0], comp[1], comp[2]]
-		ranked_comps['price_to_sales'] 	 << (comp_info << comp[3][4]['price_to_sales'][0][1])
-		ranked_comps['PEG'] 	 					 << (comp_info << comp[3][4]['PEG'][0][1])
-		ranked_comps['EBITDA'] 					 << (comp_info << comp[3][4]['EBITDA'][0][1])
+		ranked_comps['price_to_sales'] << (comp_info << comp[3][4]['price_to_sales'][0][1])
+		ranked_comps['PEG'] 	 				 << (comp_info << comp[3][4]['PEG'][0][1])
+		ranked_comps['EBITDA'] 				 << (comp_info << comp[3][4]['EBITDA'][0][1])
 	end
 
 	ranked_comps.each do |ratio, comps|
