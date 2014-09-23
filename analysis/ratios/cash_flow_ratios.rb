a7cd1e905024041667498214c107a42c8abeed78
Annika Monari <annikamonari@gmail.com>
2014-09-23 15:14:48 +0100

    ratios

--- analysis/ratios/cash_flow_ratios.rb
+++ analysis/ratios/cash_flow_ratios.rb
@@ -1,12 +1,11 @@
 def cash_flow_ratios(data)
-  cash_flow_data = Array.new(3, 0)
+  cash_flow_data = Array.new(2, 0)
 
   if data["ocf"] != nil
     ocf = data["ocf"][0]
 
     cash_flow_data[0] = ocf/data["revenue"][0] if data["revenue"] != nil
     cash_flow_data[1] = data["fcf"][0]/ocf     if data["fcf"] != nil
-    cash_flow_data[2] = ocf/data["capex"][0]   if data["capex"] != nil
   end
 
   cash_flow_data
--- analysis/ratios/debt_ratios.rb
+++ analysis/ratios/debt_ratios.rb
@@ -1,11 +1,12 @@
 def debt_ratios(data)
-  debt_data = Array.new(2, 0)
+  debt_data = Array.new(3, 0)
 
   if data["debt"] != nil
     debt = data["debt"][0]
 
     debt_data[0] = data["fcf"][0]/debt    if data["fcf"] != nil
     debt_data[1] = debt/data["equity"][0] if data["equity"] != nil
+    debt_data[2] = data['interest_expense'][0]/debt if data['interest_expense'] != nil
   end
   
   debt_data
