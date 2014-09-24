15036034a75fc2fbda7adcaeff2314a702a3c3d6
Annika Monari <annikamonari@gmail.com>
2014-09-24 18:30:17 +0100

    adsf

--- screen/process_data.rb
+++ screen/process_data.rb
@@ -111,24 +111,3 @@ def create_year_value_pairing(annual_data, group_index, ratio_index)
   end
   pairing
 end
-
-def rank_valuation_ratios(data)
-	ranked_comps = { 'price_to_sales' => [], 'PEG' => [], 'EBITDA' => [] }
-	data.each do |comp|
-    comp_info                      = [comp[0], comp[1], comp[2]]
-		ranked_comps['price_to_sales'] << (comp_info + [comp[3][5]['price_to_sales'][1]])
-		ranked_comps['PEG'] 	 			   << (comp_info + [comp[3][5]['PEG'][1]])
-		ranked_comps['EBITDA'] 				 << (comp_info + [comp[3][5]['EBITDA'][1]])
-	end
-
-	ranked_comps.each do |ratio, companies|
-    if ratio != 'PEG'
-		  ranked_comps[ratio] = companies.sort_by { |c| c[3] }
-    else
-      companies = companies.sort_by { |c| c[3] }
-      ranked_comps[ratio] = companies.reverse
-    end
-	end
-
-  ranked_comps
-end
--- screen/rank.rb
+++ screen/rank.rb
@@ -1,9 +1,23 @@
+def get_rank(companies)
+	ratio_ranks = rank_valuation_ratios(companies)
 
+	companies.each do |cik, _, _, ratios|
+		ratios.last.each do |key, val|
+			ratio_ranks[key].each do |comp_rank|
+				if cik == comp_rank[0]
+					val = ratio_ranks[key].index(comp_rank)
+					break
+				end
+			end
+		end
+	end
+	companies
+end
 
-def rank_valuation_ratios(data)
+def rank_valuation_ratios(companies)
 	ranked_comps = { 'price_to_sales' => [], 'PEG' => [], 'EBITDA' => [] }
-	data.each do |comp|
-    comp_info                      = [comp[0], comp[1], comp[2]]
+	companies.each do |comp|
+    comp_info                      =  [comp[0], comp[1], comp[2]]
 		ranked_comps['price_to_sales'] << (comp_info + [comp[3][5]['price_to_sales'][1]])
 		ranked_comps['PEG'] 	 			   << (comp_info + [comp[3][5]['PEG'][1]])
 		ranked_comps['EBITDA'] 				 << (comp_info + [comp[3][5]['EBITDA'][1]])
@@ -11,12 +25,13 @@ def rank_valuation_ratios(data)
 
 	ranked_comps.each do |ratio, companies|
     if ratio != 'PEG'
-		  ranked_comps[ratio] = companies.sort_by { |c| c[3] }
+		  ranked_comps[ratio] = (companies.sort_by { |c| c[3] }).reverse
     else
-      companies = companies.sort_by { |c| c[3] }
-      ranked_comps[ratio] = companies.reverse
+      ranked_comps[ratio] = companies.sort_by { |c| c[3] }
     end
 	end
 
   ranked_comps
 end
+
+
